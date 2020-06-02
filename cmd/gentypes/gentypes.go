4fb0d8e6d8c1d174dff09774d64d850ecd7ff9a5
dbaumgarten <mail@dbaumgarten.net>
2020-06-02 20:30:38 +0200

    Remove omitempty from ContinueResponseBody.AllThreadsContinued (#40)
    Co-authored-by: Daniel Baumgarten <incognym@googlemail.com>

--- cmd/gentypes/gentypes.go
+++ cmd/gentypes/gentypes.go
@@ -285,6 +285,11 @@ func emitToplevelType(typeName string, descJson json.RawMessage) string {
 			jsonTag := fmt.Sprintf("`json:\"%s", propName)
 			if requiredMap[propName] {
 				jsonTag += "\"`"
+			} else if typeName == "ContinueResponseBody" && propName == "allThreadsContinued" {
+				// This one special field must not have the omitempty tag, despite being optional.
+				// If this attribute is missing the client will (according to the specification) assume a value of 'true' for backward compatibility.
+				// See: https://github.com/google/go-dap/issues/39
+				jsonTag += "\"`"
 			} else {
 				jsonTag += ",omitempty\"`"
 			}
